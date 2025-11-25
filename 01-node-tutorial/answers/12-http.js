9b011cc8faede3e9c88f6566ef236f2a2b32b7b2
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-11-24 19:35:06 -0500

    fix: Revised 12-https.js

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -4,10 +4,10 @@ const server = createServer((req, res) => {
     if (req.url == '/') {
         res.end('Welcome to our home page')
     }   
-    if (req.url == '/about') {
+    else if (req.url == '/about') {
         res.end('Here is our short history')
     }
-    res.end(`
+    else res.end(`
         <h1>Oops!</h1>
         <p>We can't seem to find the page you are looking for</p>
         <a href="/">back home</a>
