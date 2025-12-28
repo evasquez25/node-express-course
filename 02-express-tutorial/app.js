3bb4e5f6ffde7e7c9ae3f7d52d554d260055fded
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-12-28 18:24:43 -0500

    Part 1 and Part 2 done. Built a basic Express program and API.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
 console.log('Express Tutorial')
+
+const express = require('express')
+const { products } = require('./data')
+const app = express()
+
+// Middleware
+app.use(express.static('./public'))
+
+// Routes 
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: 'It worked!' })
+})
+
+app.get('/api/v1/products', (req, res) => {
+    res.json(products)
+})
+
+// Handle page not found
+app.all('*', (req, res) => {
+    res.status(404).send('Page not found')
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
