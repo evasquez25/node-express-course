d297df131bbd72a3c3d507f0e82afc0d54983d27
Eric Vasquez <evasquezreyes@college.harvard.edu>
2026-01-06 19:33:12 -0500

    part 2 done. successfully implemented post method for people array. tested using postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,26 @@
 console.log('Express Tutorial')
 
 const express = require('express')
-const { products } = require('./data')
+const { products, people } = require('./data')
 const app = express()
 
-// Middleware
+// MIDDLEWARE
 const logger = (req, res, next) => {
     console.log(req.method, req.url, Date.now())
     next()
 }
- 
 app.use(logger)
-app.use(express.static('./public'))
 
-// Routes
-app.get('/api/v1/test', (req, res) => {
-    res.json({ message: 'It worked!' })
-})
+app.use(express.static('./methods-public'))
+
+// body parsers
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// GET ROUTES
+// app.get('/api/v1/test', (req, res) => {
+//     res.json({ message: 'It worked!' })
+// })
 
 app.get('/api/v1/products', (req, res) => {
     res.json(products)
@@ -29,6 +33,10 @@ app.get('/api/v1/products/:productID', (req, res) => {
         return res.status(404).json({ message: 'That product was not found.' })
     }
     res.json(product)
+})
+
+app.get('/api/v1/people', (req, res) => {
+    res.json(people)
 })
 
 app.get('/api/v1/query', (req, res) => {
@@ -50,6 +58,19 @@ app.get('/api/v1/query', (req, res) => {
         return res.json(filteredProducts.slice(0, req.query.limit))
     }
     res.json(filteredProducts)
+})
+
+// POST ROUTES
+app.post('/api/v1/people', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({ success: false, message: 'Please provide a name' })
+    }
+
+    people.push({
+        id: people.length + 1,
+        name: req.body.name
+    })
+    res.status(201).json({ success: true, name: req.body.name })
 })
 
 // Handle page not found
