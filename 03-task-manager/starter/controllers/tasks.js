070c331efefcbdd1b264b65945a74e87e515550a
Eric Vasquez <evasquezreyes@college.harvard.edu>
2026-01-17 17:11:07 -0500

    fix: handle invalid task ID with a custom Error message

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../error/custom-error')
 
 const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({})
@@ -16,9 +17,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
         const task = await Task.findOne({ _id: taskID })
 
         if (!task) {
-            const error = new Error('Not Found')
-            error.status = 404
-            return next(error)
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
         }
 
         res.status(200).json({ task })
@@ -32,7 +31,7 @@ const updateTask = asyncWrapper(async (req, res) => {
             runValidators: true,
         })
         if (!task) {
-            res.status(404).json({ msg: 'Task not found' })
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
         }
         res.status(200).json({ task })
 })
@@ -42,7 +41,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
         const task = await Task.findOneAndDelete({ _id: taskID })
         
         if (!task) {
-            return res.status(404).json({ msg: 'Task not found' })
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
         }
         
         res.status(200).json({ task })
